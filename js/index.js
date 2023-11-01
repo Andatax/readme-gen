4c77ee528dc9e32bcf372d7758ffc40c5505eac0
Andatax <abraham.mendez1011@gmail.com>
2023-10-31 20:09:28 -0600

    Added input prompts using inquirer

--- js/index.js
+++ js/index.js
@@ -1,2 +1,52 @@
 import inquirer from "inquirer";
-// console.log("Hello World");
+
+inquirer
+	.prompt([
+		{
+			type: "input",
+			name: "title",
+			message: "What is the title of your README file for your project?",
+		},
+		{
+			type: "input",
+			name: "description",
+			message: "Provide a description for your project:",
+		},
+		{
+			type: "input",
+			name: "installation",
+			message: "Provide installation instructions for your project:",
+		},
+		{
+			type: "input",
+			name: "usage",
+			message: "Enter usage information for your project:",
+		},
+		{
+			type: "list",
+			name: "license",
+			message: "Choose a license for your project:",
+			choices: ["MIT", "Apache", "GPL"],
+		},
+		{
+			type: "input",
+			name: "contributing",
+			message: "Please provide contribution guidelines for your project:",
+		},
+		{
+			type: "input",
+			name: "tests",
+			message: "Please provide instructions for testing your project.",
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "Please enter your GitHub username:",
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "Please enter your email address:",
+		},
+	])
+	.then((answers) => {});
