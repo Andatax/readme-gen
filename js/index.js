71d89a4774a75cf2d71f086860b715d71c1b83f2
Andatax <abraham.mendez1011@gmail.com>
2023-11-06 21:12:59 -0600

    Updated my code according to starter code from readme

--- js/index.js
+++ js/index.js
@@ -1,97 +1,78 @@
 import inquirer from "inquirer";
 import fs from "fs";
+import generateMarkdown from "./generateMarkdown.js";
 
-inquirer
-	.prompt([
-		{
-			type: "input",
-			name: "title",
-			message: "What is the title of your README file for your project?",
-		},
-		{
-			type: "input",
-			name: "description",
-			message: "Provide a description for your project:",
-		},
-		{
-			type: "input",
-			name: "installation",
-			message: "Provide installation instructions for your project:",
-		},
-		{
-			type: "input",
-			name: "usage",
-			message: "Enter usage information for your project:",
-		},
-		{
-			type: "list",
-			name: "license",
-			message: "Choose a license for your project:",
-			choices: ["MIT", "Apache", "GPL"],
-		},
-		{
-			type: "input",
-			name: "contributing",
-			message: "Please provide contribution guidelines for your project:",
-		},
-		{
-			type: "input",
-			name: "tests",
-			message: "Please provide instructions for testing your project.",
-		},
-		{
-			type: "input",
-			name: "github",
-			message: "Please enter your GitHub username:",
-		},
-		{
-			type: "input",
-			name: "email",
-			message: "Please enter your email address:",
-		},
-	])
-	.then(answers => {
-		const readmeContent = `
-# ${answers.title}
+// Array of questions for user input
+const questions = [
+	{
+		type: "input",
+		name: "title",
+		message: "What is the title of your README file for your project?",
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "Provide a description for your project:",
+	},
+	{
+		type: "input",
+		name: "installation",
+		message: "Provide installation instructions for your project:",
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "Enter usage information for your project:",
+	},
+	{
+		type: "list",
+		name: "license",
+		message: "Choose a license for your project:",
+		choices: ["MIT", "Apache", "GPL"],
+	},
+	{
+		type: "input",
+		name: "contributing",
+		message: "Please provide contribution guidelines for your project:",
+	},
+	{
+		type: "input",
+		name: "tests",
+		message: "Please provide instructions for testing your project.",
+	},
+	{
+		type: "input",
+		name: "github",
+		message: "Please enter your GitHub username:",
+	},
+	{
+		type: "input",
+		name: "email",
+		message: "Please enter your email address:",
+	},
+];
 
-## Description
-${answers.description}
-
-## Table of Contents
-| Section       | Description           |
-|---------------|-----------------------|
-| [Installation](#installation) | Instructions for installation. |
-| [Usage](#usage)               | How to use the project. |
-| [License](#license)           | License information. |
-| [Contributing](#contributing) | Guidelines for contributing. |
-| [Tests](#tests)               | Information on running tests. |
-| [Questions](#questions)       | Contact details for questions. |
-
-## Installation
-${answers.installation}
-
-## Usage
-${answers.usage}
-
-## License
-This project is licensed under the ${answers.license} license.
-
-## Contributing
-${answers.contributing}
-
-## Tests
-${answers.tests}
-
-## Questions
-For any additional questions, you can reach me at: [${answers.github}](https://github.com/${answers.github})  
-Email: ${answers.email}`;
-
-		fs.writeFile("README.md", readmeContent, err => {
-			err ? console.error(err) : console.log("README.md has been generated successfully!");
-		});
-	})
-	.catch(error => {
-		error.isTtyError
-			? console.log("Prompt couldn't be rendered in the current environment")
-			: console.log(error);
+// Function to write README file
+function writeToFile(fileName, data) {
+	const readmeContent = generateMarkdown(data);
+	fs.writeFile(fileName, readmeContent, err => {
+		err ? console.error(err) : console.log("README.md has been generated successfully!");
 	});
+}
+
+// Function to initialize app
+function init() {
+	inquirer
+		.prompt(questions)
+		.then(answers => {
+			writeToFile("README-Ouput/README.md", answers);
+		})
+		.catch(error => {
+			error.isTtyError
+				? console.log("Prompt couldn't be rendered in the current environment")
+				: console.log(error);
+		});
+}
+
+// Function call to initialize app
+init();
